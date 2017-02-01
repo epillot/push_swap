fbd68000c5e765ab5f86376136b1f20bbd268107
Pillot Etienne <pillotetienne@MacBook-Air-de-Pillot.local>
2017-02-01 01:48:40 +0100

    modif insertion sort

--- algo_de_tri/insertion_sort.c
+++ algo_de_tri/insertion_sort.c
@@ -13,30 +13,38 @@
 #include "libft.h"
 #include "checker.h"
 
-static void	insert_elem(t_list **l, t_list *elem, int size)
+static void	insert_elem(t_list **l, t_list *elem)//, int size)
 {
 	int		i;
 	t_list	*tmp;
+	t_list	*tmp_prev;
 
 	i = 1;
+	tmp_prev = NULL;
 	tmp = *l;
-	while (i <= size && VALUE(tmp) < VALUE(elem))
-	{
+	while (tmp && VALUE(elem) >= VALUE(tmp))
+	{	
+		tmp_prev = tmp;
 		tmp = tmp->next;
-		i++;
 	}
-	ft_lst_insert(l, elem, i);
+	if (tmp_prev)
+	{
+		tmp_prev->next = elem;
+		elem->next = tmp;
+	}
+	else
+		ft_lstadd(l, elem);
 }
 
 void    print_l(t_list *l);
 
-static void	del(void *content, size_t content_size)
+void	del(void *content, size_t content_size)
 {
 	(void)content_size;
 	free(content);
 }
 
-void		insertion_sort(t_list **l)
+/*void		insertion_sort(t_list **l)
 {
 	t_list	*sortedlist;
 	t_list	*tmplist;
@@ -58,28 +66,33 @@ void		insertion_sort(t_list **l)
 	}
 	ft_lstdel(l, del);
 	*l = sortedlist;
-}
+}*/
 
 
-/*void		insertion_sort(t_list **l)
+void		insertion_sort(t_list **l)
 {
 	t_list	*tmplist;
 	t_list	*tmp;
-	int		size;
 
 	if (!*l)
 		return ;
 	tmplist = *l;
-	tmp = (*l)->next;
-	size = 1;
+//	ft_putchar('\n');
 	while (tmplist->next)
 	{
-		tmp = tmplist->next;
-		tmplist->next = tmp->next;
-//		print_l(*l);
-		insert_elem(l, tmp, size);
-//		print_l(*l);
-		size++;
-		tmplist = ft_lst_at(*l, size);
+		if (VALUE(tmplist) > VALUE(tmplist->next))
+		{
+			tmp = tmplist->next;
+			tmplist->next = tmp->next;
+//			ft_printf("on prend le maillon: %d\n", VALUE(tmp));
+//			print_l(*l);
+//			ft_printf("on l'insert avec size = %d\n", size);
+			insert_elem(l, tmp);
+//			print_l(*l);
+//			ft_putchar('\n');
+		}
+		else	
+			tmplist = tmplist->next;
+	//	tmplist = ft_lst_at(*l, size);
 	}
-}*/
+}
